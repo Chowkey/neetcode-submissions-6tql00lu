8ef0f80a99295e61dc6fb31a37e8c8d91f59b4f6
Chow <161998959+Chowkey@users.noreply.github.com>
2026-05-28 18:12:21 +0700

    Add: longest-repeating-substring-with-replacement - submission-2

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-2.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        //the char with most count in the substring must - total count of other different char is <= k
+        //
+        unordered_map<char, int> mp;
+        int l = 0;
+        int r = 0;
+        int res = 0;
+        char maxc = s[0];
+        for(int i = 0; i < s.size(); i++) {
+            if (mp.contains(s[i])) {
+                mp[s[i]]++;
+            }
+            else mp[s[i]] = 1;
+            if (mp[s[i]] >= mp[maxc]) maxc = s[i];
+            r = i ;
+            while (r-l+1 > mp[maxc] + k ) {
+                mp[s[l]]--;
+                for(auto x: mp) {
+                    if (x.second > mp[maxc]) maxc = x.first;
+                }
+                l++;
+            }
+            cout << r <<  " " << l << endl;
+            res = max(res, r-l+1);
+        }
+        return res;
+    }
+};
