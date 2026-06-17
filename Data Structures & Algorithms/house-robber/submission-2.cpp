3c6d31b5706efbc6844dc9c709ac2e4974e4b1ea
Chow <161998959+Chowkey@users.noreply.github.com>
2026-06-18 00:35:38 +0700

    Add: house-robber - submission-2

---
+++ Data Structures & Algorithms/house-robber/submission-2.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int rob1 = 0; //can be added
+        int rob2 = 0; // cant be added
+        for(int i = 0 ; i < nums.size(); i++) {
+           int rob  = rob1+ nums[i];
+            rob1 = max(rob1,rob2);
+            rob2 = rob;
+        }
+        return max(rob2,rob1);
+    }
+};
